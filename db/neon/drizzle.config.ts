0da124f8df1eb5b95a17a571d8db5c519e9573bb
suman-3 <mondalsumanrock20@gmail.com>
2025-10-01 16:13:54 +0530

    chore: update .gitignore and drizzle configuration, remove obsolete SQL and metadata files

--- db/neon/drizzle.config.ts
+++ db/neon/drizzle.config.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
+  out: './migrations/local',
   schema: './src/db/schema.ts',
-  dialect: "postgresql",
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
+  verbose: true,
+  strict: true,
 });
